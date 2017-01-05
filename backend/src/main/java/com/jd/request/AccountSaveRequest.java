3bf8186f2efc59991eac637e2c1fdca9bc7dc962
gyp <gyp@ebfc5a33-59e9-4177-ba62-353ae4692a5c>
2017-01-05 08:25:15 +0000

    账户模块联调

---
+++ backend/src/main/java/com/jd/request/AccountSaveRequest.java
@@ -0,0 +1,47 @@
+package com.jd.request;
+
+/**
+ * Created by renchao on 2017/1/5.
+ */
+public class AccountSaveRequest {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String roleIds;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoleIds() {
+        return roleIds;
+    }
+
+    public void setRoleIds(String roleIds) {
+        this.roleIds = roleIds;
+    }
+}
