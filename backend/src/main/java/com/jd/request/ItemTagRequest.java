c2d6cf18a81195c95c13ffb57980382605161d62
gyp <gyp@ebfc5a33-59e9-4177-ba62-353ae4692a5c>
2017-01-06 12:40:23 +0000

    权限操作修正

---
+++ backend/src/main/java/com/jd/request/ItemTagRequest.java
@@ -0,0 +1,71 @@
+package com.jd.request;
+
+/**
+ * @author Ellen.
+ * @date 2017/1/6.
+ * @since 1.0.
+ * com.jd.request .by jingdun.tech.
+ */
+public class ItemTagRequest {
+
+    private Long id;
+
+    private String name;
+
+    private String bannerId;
+
+    private String desc;
+
+    private Integer sequence;
+
+    private Boolean visible;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getBannerId() {
+        return bannerId;
+    }
+
+    public void setBannerId(String bannerId) {
+        this.bannerId = bannerId == null ? null : bannerId.trim();
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc == null ? null : desc.trim();
+    }
+
+    public Integer getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(Integer sequence) {
+        this.sequence = sequence;
+    }
+
+    public Boolean getVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+}
